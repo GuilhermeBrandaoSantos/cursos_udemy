4278f6a49598b48a464090ceffbc56fc2c21545c
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2020-04-10 20:09:36 -0300

    Criando metodo para sempre que eu alterar o input, o tirulo será alterado também. Adicionei o bootstrap.

--- front_end/vue_js/introducao/primeiro_app.php
+++ front_end/vue_js/introducao/primeiro_app.php
@@ -3,20 +3,38 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <title>Curso de VueJS (UDEMY)</title>
     </head>
+    <style>
+        #app{margin-top: 150px; display:flex; align-items: center;}        
+    </style>
     <body>
-        <div id="app">
-            <h1>{{ titulo }}</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div id="app">
+                        <input type="text" v-on:input="alterarTitulo">
+                        <h1>{{ titulo }}</h1>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
         <script>
             new Vue({
                 el: '#app',
                 data: {
                     titulo: 'Hello world!!!'
+                },
+                methods:{
+                    alterarTitulo(event){
+                        this.titulo = event.target.value
+                    }
                 }
             });
 
