ec6066a02d6be4ec0e5d9908e59a2b903c38680f
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2020-04-11 17:18:09 -0300

    Correção do desafio

--- front_end/vue_js/dom/desafio-01/app.js
+++ front_end/vue_js/dom/desafio-01/app.js
@@ -4,11 +4,14 @@ new Vue({
         nome: 'Guilherme Brandão dos Santos',
         idade: 23,
         linkImage: 'https://avatarfiles.alphacoders.com/182/182818.jpg',
-        numeroRangdon: Math.random(0 , 1)
+        numeroRangdon: Math.random(0 , 1)  // assim funciona
     },
     methods: {
-        idadeMultiplica: function(){
+        idadeMultiplica(){
             return this.idade * 3
-        }     
+        },
+        random(){
+            return Math.random(0 , 1) // Quando utilizo dessa forma, não funciona
+        }    
     }
 })
