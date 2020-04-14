550e38f99e8d111046ee9472ef9592193f56f194
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2020-04-13 23:53:23 -0300

    Resolvendo desafio 3

--- front_end/vue_js/dom/desafio-03/app.js
+++ front_end/vue_js/dom/desafio-03/app.js
@@ -2,5 +2,17 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0
+    },
+    watch:{
+        valor(){
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000);
+        }
+    },
+    computed:{
+        resultado(){
+            return this.valor == 37 ? 'Valor Igual':'Valor Diferente'
+        }
     }
 });
